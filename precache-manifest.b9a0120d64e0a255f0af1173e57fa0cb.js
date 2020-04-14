e1ca1ddc07bccdda9769b7ba770e7e9a2a0421e6
joyfulben <benfishernow@gmail.com>
2020-04-14 09:01:08 -0600

    changed resume height

--- precache-manifest.b9a0120d64e0a255f0af1173e57fa0cb.js
+++ precache-manifest.b9a0120d64e0a255f0af1173e57fa0cb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a57757a3041185a54a0084d05e5d1c15",
+    "revision": "ff024c9634eec93f2bd175bc166f2ff1",
     "url": "/index.html"
   },
   {
-    "revision": "a112fd5754a0f0b783c7",
+    "revision": "484169b0c4242a5566a9",
     "url": "/static/css/main.01a885ef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f2a0b89a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a112fd5754a0f0b783c7",
-    "url": "/static/js/main.0a1ce1f7.chunk.js"
+    "revision": "484169b0c4242a5566a9",
+    "url": "/static/js/main.c6020d7d.chunk.js"
   },
   {
     "revision": "5284eec07972ace79c95",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.357c95a2cef502727f2abc4b4bc0d353.js"
+  "/precache-manifest.b9a0120d64e0a255f0af1173e57fa0cb.js"
 );
 
 self.addEventListener('message', (event) => {
